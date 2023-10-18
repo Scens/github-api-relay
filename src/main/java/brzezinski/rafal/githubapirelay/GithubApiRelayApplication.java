a741260629ac84fc6510489ace6df27fbb9e0e15
r.brzezinski <rafalbrzez@gmail.com>
2023-10-18 20:19:55 +0200

    Initial commit

---
+++ src/main/java/brzezinski/rafal/githubapirelay/GithubApiRelayApplication.java
@@ -0,0 +1,13 @@
+package brzezinski.rafal.githubapirelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiRelayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brzezinski/rafal/githubapirelay/GithubApiRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package brzezinski.rafal.githubapirelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiRelayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
